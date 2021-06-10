27e335a97f541f7b9357b793aac26f26cbbaceec
Sylkyu <miiyusli@gmail.com>
2021-06-10 17:21:06 +1200

    use event enum for AudioObjectCreationWindow

--- Assets/Editor/AudioObjectCreationWindow.cs
+++ Assets/Editor/AudioObjectCreationWindow.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
-using System;
+using Assets.Scripts;
 
 public class AudioObjectCreationWindow : EditorWindow
 {
-    public GameEventBlackboard GameEventBlackboard;
-    int selected = 0;
+    GameEvent2 selected;
     string ObjectName = string.Empty;
     AudioClip Clip;
     string TriggerEventKey = "";
@@ -15,31 +12,26 @@ public class AudioObjectCreationWindow : EditorWindow
     float Volume = 1;
     float Pitch = 1;
     float StereoPan = 0;
-   float SpatialBlend = 0;
+    float SpatialBlend = 0;
     float ReverbZoneMix = 1;
+
+    private readonly string _outputFilePathFormat = "Assets/Scripts/{0}.asset";
+
     [MenuItem("Tools/AudioObject Creation Window")]
+
     public static void Open()
     {
         GetWindow<AudioObjectCreationWindow>();
     }
+
     private void OnGUI()
     {
-        GameEventBlackboard = (GameEventBlackboard)EditorGUILayout.ObjectField(GameEventBlackboard, typeof(GameEventBlackboard), true);
-        if (GameEventBlackboard == null)
-        {
-            return;
-        }
         GUILayout.BeginVertical();
         GUILayout.Label("Object Name:");
         ObjectName = GUILayout.TextField(ObjectName, 20);
         GUILayout.Label("Audio Clip");
         Clip = (AudioClip)EditorGUILayout.ObjectField(Clip,typeof(AudioClip),true);
-        string[] options = new string[GameEventBlackboard.ExistingKeys.Count];
-        for (int i = 0; i < options.Length; i++)
-        {
-            options[i] = GameEventBlackboard.ExistingKeys[i];
-        }
-        selected = EditorGUILayout.Popup("Event Trigger Options",selected, options);
+        selected = (GameEvent2)EditorGUILayout.EnumPopup("Event Trigger Options",selected);
         GUILayout.Label("Audio Priorty:");
         Priority = EditorGUILayout.IntSlider(Priority, 0, 256);
         GUILayout.Label("Audio Volume:");
@@ -54,7 +46,7 @@ public class AudioObjectCreationWindow : EditorWindow
         ReverbZoneMix = EditorGUILayout.Slider(ReverbZoneMix, 0f, 1.1f);
         if(GUILayout.Button("Create Audio Object"))
         {
-            TriggerEventKey = string.Copy(options[selected]);
+            TriggerEventKey = selected.ToString();
             CreateAudioObject();
         }
         GUILayout.EndVertical();
@@ -64,8 +56,7 @@ public class AudioObjectCreationWindow : EditorWindow
     {
         AudioObject NewObject = ScriptableObject.CreateInstance<AudioObject>();
         NewObject.name = ObjectName;
-        string path = "Assets/Scripts/"
-            + NewObject + ".asset";
+        var path = string.Format(_outputFilePathFormat, NewObject);
         NewObject.FillAudioObject(Clip, TriggerEventKey, Priority, Volume, Pitch, StereoPan, SpatialBlend, ReverbZoneMix);
         AssetDatabase.CreateAsset(NewObject, path);
         EditorUtility.SetDirty(NewObject);
