b8223742f7e5cde8bcf1c7b5ec8b9d359b98f2b3
Sylkyu <miiyusli@gmail.com>
2021-06-12 19:53:30 +1200

    refactor rope to use rigidbody instead of character as target

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -4,26 +4,23 @@ namespace Assets.Scripts
 {
     public class Rope : MonoBehaviour
     {
-        [SerializeField] GameObject ConnectedTo;
+        [SerializeField] Rigidbody2D ConnectedToRigidBody;
         [SerializeField] float MaxLength = 5;
         [SerializeField] float PullStrength = 3;
 
         // When false, ConnectedTo will be pulled. When true, this will be pulled.
         private bool _ropeFlipped = true;
         private bool _maxLengthReached;
+        private Color _ropeOriginalColor;
 
         private Rigidbody2D _thisRigidbody2D;
-        private Color _thisOriginalColor;
-        private Rigidbody2D _otherRigidBody2D;
-        private Color _otherOriginalColor;
         private LineRenderer _lineRenderer;
 
-        Color _ropeOriginalColor;
-        //Z: Added so that the CharacterWallCollision can check if it is at max.
-        public bool CheckForMaxLength()
+        public bool MaxLengthReached()
         {
             return _maxLengthReached;
         }
+
         public void FlipRopeTarget()
         {
             _ropeFlipped = !_ropeFlipped;
@@ -32,9 +29,6 @@ namespace Assets.Scripts
         private void Start()
         {
             _thisRigidbody2D = GetComponent<Rigidbody2D>();
-            _thisOriginalColor = GetComponent<SpriteRenderer>().color;
-            _otherRigidBody2D = ConnectedTo.GetComponent<Rigidbody2D>();
-            _otherOriginalColor = ConnectedTo.GetComponent<SpriteRenderer>().color;
             _lineRenderer = GetComponent<LineRenderer>();
             _ropeOriginalColor = _lineRenderer.startColor;
         }
@@ -52,7 +46,7 @@ namespace Assets.Scripts
 
         private void CheckDistance()
         {
-            var distance = Vector2.Distance(this.transform.position, ConnectedTo.transform.position);
+            var distance = Vector2.Distance(this.transform.position, ConnectedToRigidBody.transform.position);
             _maxLengthReached = distance > MaxLength;
         }
 
@@ -63,21 +57,21 @@ namespace Assets.Scripts
             if (_ropeFlipped)
             {
                 // apply forces - only on this object
-                var direction = ConnectedTo.transform.position - this.transform.position;
+                var direction = ConnectedToRigidBody.transform.position - this.transform.position;
                 _thisRigidbody2D.AddForce(direction.normalized * PullStrength);
             } 
             else
             {
                 // apply forces - only on the connected object
-                var direction = this.transform.position - ConnectedTo.transform.position;
-                _otherRigidBody2D.AddForce(direction.normalized * PullStrength);
+                var direction = this.transform.position - ConnectedToRigidBody.transform.position;
+                ConnectedToRigidBody.AddForce(direction.normalized * PullStrength);
             }
         }
 
         private void DrawRope()
         {
             _lineRenderer.SetPosition(0, this.transform.position);
-            _lineRenderer.SetPosition(1, ConnectedTo.transform.position);
+            _lineRenderer.SetPosition(1, ConnectedToRigidBody.transform.position);
             if (_maxLengthReached)
             {
                 _lineRenderer.startColor = Color.red;
@@ -86,7 +80,6 @@ namespace Assets.Scripts
             }
             _lineRenderer.startColor = _ropeOriginalColor;
             _lineRenderer.endColor = _ropeOriginalColor;
-
         }
     }
 }
