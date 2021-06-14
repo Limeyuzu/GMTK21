670657fd6ac38bf3c5a012cd3d30f535a1255d52
Sylkyu <miiyusli@gmail.com>
2021-06-14 22:18:54 +1200

    change controls to space zxc

--- Assets/Scripts/PlayerControlKeyCodes.cs
+++ Assets/Scripts/PlayerControlKeyCodes.cs
@@ -1,18 +1,17 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class PlayerControlKeyCodes
     {
-        public static KeyCode MOVE_UP = KeyCode.W;
-        public static KeyCode MOVE_DOWN = KeyCode.S;
-        public static KeyCode MOVE_LEFT = KeyCode.A;
-        public static KeyCode MOVE_RIGHT = KeyCode.D;
+        public static KeyCode MOVE_UP = KeyCode.UpArrow;
+        public static KeyCode MOVE_DOWN = KeyCode.DownArrow;
+        public static KeyCode MOVE_LEFT = KeyCode.LeftArrow;
+        public static KeyCode MOVE_RIGHT = KeyCode.RightArrow;
 
-        public static KeyCode SPECIAL_ABILITY = KeyCode.LeftShift;
-        public static KeyCode PULL_ROPE = KeyCode.E;
-        public static KeyCode SWITCH_CHARACTER = KeyCode.Q;
+        public static KeyCode SPECIAL_ABILITY = KeyCode.C;
+        public static KeyCode PULL_ROPE = KeyCode.X;
+        public static KeyCode SWITCH_CHARACTER = KeyCode.Z;
         public static KeyCode THROW_OBJECT = KeyCode.Space;
 
         public static KeyCode DETACH_ROPE = KeyCode.None;
